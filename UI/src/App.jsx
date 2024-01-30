b4121e819408405a3e77f8a61ee6ef7fbfc07f55
hasan-kayan <hasankayan2000@hotmail.com>
2024-01-30 10:17:42 +0300

    State

---
+++ UI/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import AuthenticationPage from './components/initialPage'
+
+function App() {
+  
+
+  return (
+   
+      <div>
+         <AuthenticationPage/>
+      </div>
+
+  )
+}
+
+export default App
